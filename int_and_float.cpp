30925c0cddc8f31ef278d078db7a52332b306cca
Rifatul Islam <140544121+rifatulislaminfo@users.noreply.github.com>
2023-09-25 23:52:03 +0600

    Add files via upload

---
+++ int_and_float.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+ 
+double a;
+
+cin>>a;
+
+int c=a;
+if(a==c){
+	cout<<"int "<<c<<endl;
+
+}
+
+else{
+
+	int k=a;
+	double s=a-k;
+
+	cout<<"float "<<k<<" "<<s<<endl;
+
+ }
+ 
+
+ return 0;
+
+}
